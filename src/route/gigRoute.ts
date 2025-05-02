31e1b02ce1fd1d189dd7d27cc69d08a04f2da125
El Houcine Qara <elhoucine.qara@harx.ai>
2025-05-02 12:16:56 +0100

    Update dockerfile

--- src/route/gigRoute.ts
+++ src/route/gigRoute.ts
@@ -6,6 +6,8 @@ const router = express.Router();
 router.post("/", GigController.createGig);
 router.get("/", GigController.getAllGigs);
 router.get("/:id", GigController.getGigById);
+
+// Gig Destination Zone
 router.get("/:id/destination-zone", GigController.getGigDestinationZoneById);
 router.put("/:id", GigController.updateGig);
 router.delete("/:id", GigController.deleteGig);
