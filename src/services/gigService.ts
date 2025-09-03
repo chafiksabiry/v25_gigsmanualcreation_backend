c9c5d2925a9dbc95f32fdb590564db90ecd84c6c
El Houcine Qara <elhoucine.qara@harx.ai>
2025-09-03 14:16:13 +0100

    Update gigs model

--- src/services/gigService.ts
+++ src/services/gigService.ts
@@ -157,7 +157,13 @@ export class GigService {
         throw new Error("Invalid Company ID format");
       }
 
-      const gigs = await Gig.find({ companyId });
+      const gigs = await Gig.find({ companyId })
+        .populate('activities')
+        .populate('industries')
+        .populate('skills.professional.skill')
+        .populate('skills.technical.skill')
+        .populate('skills.soft.skill')
+        .populate('skills.languages.language');
       return gigs;
     } catch (error) {
       console.error("Error in getGigsByCompanyId:", error);
